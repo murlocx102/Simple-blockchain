8035643c240c588318cf95e39dcd8e1135968bcd
Murloc <37508939+murlocx102@users.noreply.github.com>
2020-09-29 17:49:00 +0400

    Update business.go

--- wallet/business.go
+++ wallet/business.go
@@ -157,3 +157,29 @@ func (b *Business) CoinOptData(toAddress common.Address, value float64) []byte {
 
 	return data
 }
+
+// 将小数iamount转换为wei(整数),decimals是小数位数。
+func ToWei(iamount interface{}, decimals int) *big.Int {
+	amount := decimal.NewFromFloat(0)
+    
+	switch v := iamount.(type) {
+	case string:
+		amount, _ = decimal.NewFromString(v)
+	case float64:
+		amount = decimal.NewFromFloat(v)
+	case int64:
+		amount = decimal.NewFromFloat(float64(v))
+	case decimal.Decimal:
+		amount = v
+	case *decimal.Decimal:
+		amount = *v
+	}
+
+	mul := decimal.NewFromFloat(float64(10)).Pow(decimal.NewFromFloat(float64(decimals)))
+	result := amount.Mul(mul)
+
+	wei := new(big.Int)
+	wei.SetString(result.String(), 10)
+
+	return wei
+}
